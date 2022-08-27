b6e2960f78216192cad5a4bb2e0ed50c27ee84dd
Muzammil <muzamilbhundi@gmail.com>
2022-08-28 01:30:28 +0500

    result length 0 resolved

--- routes/admin.js
+++ routes/admin.js
@@ -1,46 +1,53 @@
-import { Router } from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import db from '../libs/db.js'
+import { Router } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import db from "../libs/db.js";
 // const secret = proces.env.JWT_SECRET;
-const secret = 'nodejs';
+const secret = "nodejs";
 const router = Router();
 
-router.post('/signup', async (req,res)=>{
-    let sql = 'INSERT INTO admin SET ?';
-    // console.log(req.body.password)
-    const password = bcrypt.hashSync(req.body.password,10);
-    const obj = {
-        name:req.body.name,
-        email:req.body.email,
-        password: password
+router.post("/signup", async (req, res) => {
+  let sql = "INSERT INTO admin SET ?";
+  // console.log(req.body.password)
+  const password = bcrypt.hashSync(req.body.password, 10);
+  const obj = {
+    name: req.body.name,
+    email: req.body.email,
+    password: password,
+  };
+  db.query(sql, obj, (err) => {
+    if (err) {
+      res.status(400).json({ message: err.message });
     }
-    db.query(sql,obj,(err)=>{
-        if(err){
-            res.status(400).json({message:err.message});
-        }
-        res.status(200).json({message:'Admin created successfully'});
-    })
-})
+    res.status(200).json({ message: "Admin created successfully" });
+  });
+});
 
-router.post('/login',(req,res)=>{
-    let sql = `SELECT * FROM admin WHERE email = '${req.body.email}'`;
-    db.query(sql,async(err,result)=>{
-        if(err){
-            res.status(401).json({message:'Wrong credentials'});
-        } else{
-            console.log(result[0].password)
-            const match = await bcrypt.compare(req.body.password,result[0].password);
-            if(match){
-                jwt.sign(JSON.stringify(result[0]),secret,(err,token)=>{
-                    if(err) throw err;
-                    res.status(200).json({token,message:"Login successfully"});
-                })
-            }else{
-                res.status(401).json({message:'Wrong credentials'});
-            }
+router.post("/login", (req, res) => {
+  let sql = `SELECT * FROM admin WHERE email = '${req.body.email}'`;
+  db.query(sql, async (err, result) => {
+    if (err) {
+      res.status(401).json({ message: "Wrong credentials" });
+    } else {
+      if (result.length > 0) {
+        console.log(result.length);
+        const match = await bcrypt.compare(
+          req.body.password,
+          result[0].password
+        );
+        if (match) {
+          jwt.sign(JSON.stringify(result[0]), secret, (err, token) => {
+            if (err) throw err;
+            res.status(200).json({ token, message: "Login successfully" });
+          });
+        } else {
+          res.status(401).json({ message: "Wrong credentials" });
         }
-    })
-})
+      } else {
+        res.status(401).json({message:'email not found'})
+      }
+    }
+  });
+});
 
 export default router;
